4fbb3947cc35dac0f276bc64ed35fc95a59db9ac
Rashed-Jaman-Raj <rashed.jaaman@gmail.com>
2026-01-14 00:01:46 +0600

    product card

--- src/pages/shop/ProductCard.jsx
+++ src/pages/shop/ProductCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { getImgUrl } from '../../utils/getImageUrl'
+import Rating from '../../components/navbar/Rating'
+import { FiPlus } from "react-icons/fi";
 
 const ProductCard = ({product}) => {
   return (
-    <div>
-      <div className='bg-[#FAFAFA]'>
+    <div className='border border-gray-300 rounded-md hover:shadow-lg duration-200 overflow-hidden'>
+      <div className='hover:scale-104 duration-200 cursor-pointer'>
         <img src={getImgUrl(`${product.imageUrl}`)} alt="" />
+      </div>
+      <div className='p-6'>
+        <h4 className='text-base mb-1'>{product.category}</h4>
+        <h3 className='font-semibold text-xl mb-2'>{product.name}</h3>
+        <Rating rating={product.rating}/>
+        <div className=' mt-5 flex justify-between items-center'>
+          <p className='text-gray-950 font-bold text-lg'><sup>$</sup> <span>{product.price}</span></p>
+            <button className='bg-gray-950 text-white hover:bg-black/55 p-2 rounded-full cursor-pointer'>
+            <FiPlus />
+            </button>
+        </div>
       </div>
     </div>
   )
--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -1,39 +1,69 @@
-import React from 'react'
-import { products } from '../../utils/Products'
-import ProductCard from './ProductCard'
+import React, { useState } from "react";
+import { products } from "../../utils/Products";
+import ProductCard from "./ProductCard";
+import btnIcon from "../../assets/button-icon.png"
 
-const Products = ({headline}) => {
-  const categories = ["Chair", "Beds", "Sofa", "Lamp"]
+const Products = ({ headline }) => {
+  const categories = ["Chair", "Beds", "Sofa", "Lamp"];
+  const [selectedCategory, setSelectedCategory] = useState("Chair");
+  const [visibleProducts, setVisibleProducts] = useState(4);
+  const filteredProducts = products.filter(
+    (product) => product.category === selectedCategory
+  );
+  const loadMoreProducts = () => {
+    setVisibleProducts((prev) => prev + 4);
+  };
   return (
-    <div>
-      <div className='section-container'>
-        <h2 className='text-4xl font-bold text-center my-8 '>{headline}</h2>
+    <div className="px-12">
+      <div className="max-w-screen-2x1 mx-auto">
+        <h2 className="text-4xl font-bold text-center my-8 ">{headline}</h2>
 
         {/* category tabs */}
-        <div className='bg-[#EEEEEE] max-w-md mx-auto sm:rounded-full md-p-2.5 py-4 px-2 mb-16 '>
-          <div className='flex flex-col sm:flex-row items-center md:justify-between justify-center gap-4'>
-            {
-              categories.map((category) => (
-                <button key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-amber-500 hover:text-white transition-colors cursor-pointer`}>{category}</button>
-              ))
-            }
+        <div className="bg-[#EEEEEE] max-w-md mx-auto sm:rounded-full md-p-2.5 py-2 px-2 mb-16 ">
+          <div className="flex flex-col sm:flex-row items-center md:justify-between justify-center gap-4">
+            {categories.map((category) => (
+              <button
+                onClick={() => {
+                  setSelectedCategory(category);
+                  setVisibleProducts(4);
+                }}
+                key={category}
+                className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-amber-500 hover:text-white transition-colors cursor-pointer ${
+                  selectedCategory === category
+                    ? "bg-white text-amber-500"
+                    : " text-black"
+                }`}
+              >
+                {category}
+              </button>
+            ))}
           </div>
         </div>
 
         <div>
-            {/* products grid */}
-            <div>
-              {
-                products.map((product, index) => (
-                  <ProductCard key={index} product={product}/>
-                ))
-              }
+          {/* products grid */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 max-w-screen-2x1 container mx-auto">
+            {filteredProducts
+              .slice(0, visibleProducts)
+              .map((product, index) => (
+                <ProductCard key={index} product={product} />
+              ))}
+          </div>
+          {/* load more button */}
+          {visibleProducts < filteredProducts.length && (
+            <div className="flex justify-center items-center mt-8">
+              <button 
+               onClick={loadMoreProducts}
+              className="text-base font-semibold text-amber-500 flex items-center cursor-pointer">
+                View All
+                <img src={btnIcon} alt="btn icon" />
+              </button>
             </div>
-
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
